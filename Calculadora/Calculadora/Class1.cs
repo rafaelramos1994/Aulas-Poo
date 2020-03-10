593e50a69160db6ee6fe1e5e5f12901d9affded7
rafaelramos1994 <61336330+rafaelramos1994@users.noreply.github.com>
2020-03-10 19:45:38 -0300

    Add files via upload

---
+++ Calculadora/Calculadora/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        private List<double> listnumero = new List<double>();
+
+
+        public void addnumero(double num)
+        {
+            this.listnumero.Add(num);
+        }
+
+        public double somar()
+        {
+            double resultado = 0;
+            foreach (double numero in this.listnumero)
+            {
+                resultado += numero;
+            }
+            this.listnumero.Clear();
+            return resultado;
+
+        }
+
+        public double subtrair()
+        {
+            double resultado = this.listnumero[0];
+            this.listnumero.RemoveAt(0);
+            foreach (double numero in this.listnumero)
+            {
+                resultado -= numero;
+            }
+            this.listnumero.Clear();
+            return resultado;
+
+        }
+
+        public double mult()
+        {
+            double resultado = this.listnumero[0];
+            this.listnumero.RemoveAt(0);
+            foreach (double numero in this.listnumero)
+            {
+                resultado *= numero;
+            }
+            this.listnumero.Clear();
+            return resultado;
+
+        }
+
+        public double divisao()
+        {
+            double resultado = this.listnumero[0];
+            this.listnumero.RemoveAt(0);
+            foreach (double numero in this.listnumero)
+            {
+                resultado /= numero;
+            }
+            this.listnumero.Clear();
+            return resultado;
+
+        }
+
+        
+
+    }
+}
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+
+            Console.WriteLine("Digite os numeros para somar ");
+            c.addnumero(Convert.ToDouble(Console.ReadLine()));
+            c.addnumero(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine(c.somar());
+
+            Console.WriteLine("Digite os numeros para subitrair ");
+            c.addnumero(Convert.ToDouble(Console.ReadLine()));
+            c.addnumero(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine(c.subtrair());
+
+            Console.WriteLine("Digite os numeros para multiplicar ");
+            c.addnumero(Convert.ToDouble(Console.ReadLine()));
+            c.addnumero(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine(c.mult());
+
+            Console.WriteLine("Digite os numeros para dividir ");
+            c.addnumero(Convert.ToDouble(Console.ReadLine()));
+            c.addnumero(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine(c.divisao());
+
+            Console.ReadKey();
+        }
+    }
+}
